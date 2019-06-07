39c54316e257f6a981f5cdba049826ac5a4e9f22
the mongesan <37490690+mongesan@users.noreply.github.com>
2019-06-07 21:56:42 +0900

    a

---
+++ v0a/main.js
@@ -0,0 +1,9 @@
+/*      画面サイズの取得        */
+
+/*function getPositionHeight(){
+    const sh = Math.ceil(window.innerHeight / 10) * 10;
+    console.log(sh);
+}
+
+window.onresize = function(){getPositionHeight();} */
+
